7fab3477c795cf815e71a069b76ac6a602cbb31c
Rehana Abdulber <rabdulber@excellerentsolutions.com>
2021-05-17 03:39:05 +0300

    initial commit

---
+++ src/containers/login/login.jsx
@@ -0,0 +1,79 @@
+import { Form, Input, Button, Checkbox } from "antd";
+import { Container } from "../../../components/Container/Container";
+import { useHistory } from "react-router-dom";
+import { SectionHeading } from "../../../components/SectionHeading/SectionHeading";
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+export const Login = () => {
+  const history = useHistory();
+
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  const handleRegister = () =>{
+    let path = `/register`;
+    history.push(path);
+  }
+
+  return (
+    <Container>
+            <SectionHeading heading="Login to Your Account"/>
+
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: "Please input your username!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please input your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+    </Container>
+  );
+};
