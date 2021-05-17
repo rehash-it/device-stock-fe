e5209b331df2f9a94d8e2bc1aa10b7fdd9383192
Rehana Abdulber <rabdulber@excellerentsolutions.com>
2021-05-17 03:44:36 +0300

    added utilitiy function for authentication

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import PrivateRoute from "./utils/PrivateRoute";
+import PublicRoute from "./utils/PublicRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Switch>
+          <PublicRoute exact path="/" component={CustomerLayout} />
+            <PublicRoute path="/admin/orgservice/:id" component= {OrganizationService}/>
+            <PublicRoute path="/admin/organizations" component={Organization} />
+        </Switch>
+      </Router>
   );
 }
 
---
+++ utils/PublicRoute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { getToken } from './common';
+ 
+function PublicRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) => !getToken() ? <Component {...props} /> : <Redirect to={{ pathname: '/' }} />}
+    />
+  )
+}
+ 
+export default PublicRoute;
